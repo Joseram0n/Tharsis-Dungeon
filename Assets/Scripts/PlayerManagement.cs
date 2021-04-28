b5acdc4d31c060d7200f1dc1495ca866fe4e256a
manureyes99@gmail.com <>
2021-04-28 21:36:44 +0200

    Player more modular, DontDestroyOnLoad

--- Assets/Scripts/PlayerManagement.cs
+++ Assets/Scripts/PlayerManagement.cs
@@ -4,37 +4,184 @@ using UnityEngine;
 
 public class PlayerManagement : MonoBehaviour
 {
-    public int maxHealth = 6;
+    public int maxHealth = 100;
     public int currentHealth;
 
-    public HealthBar healthbar;
+    public Canvas canvas;
     // Start is called before the first frame update
+
+
+    private Vector2 direction;
+    private Vector2 AttackDir;
+    private Animator animator;
+
+    private SpriteRenderer spriterenderer;
+    public Sprite arco;
+    public Sprite sword;
+    private bool isSword = true;
+    private bool imAttacking = false;
+
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    private Transform attackPoint;
+    public float multiplier;
+    public int damage = 20;
+
+    public GameObject arrow;
+    public float projectileForce = 2f;
+    private Rigidbody2D rb;
+
+    private PlayerMovement pm;
+
+
+
+
     void Start()
     {
+        //Vida, items
         currentHealth = maxHealth;
-        healthbar.setMaxHealth(maxHealth);
+        canvas.GetComponent<DisplayManagement>().setMaxHealth(maxHealth);
+
+        //Movimiento
+        animator = GetComponent<Animator>();
+        AttackDir = Vector2.zero;
+        pm = GetComponent<PlayerMovement>();
+
+        //Cambiar de Sprite
+        spriterenderer = this.GetComponent<SpriteRenderer>();
+        spriterenderer.sprite = sword;
+
+        //Punto de ataque de la espada
+        GameObject newGameObject = new GameObject();
+        attackPoint = newGameObject.transform;
+        Destroy(newGameObject);
+
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-            takeDamage();
-        if (Input.GetKeyDown(KeyCode.H))
-            heal();
-        
+        if (!CheckAttack())
+        {
+            TakeInput();
+        }
+        else
+        {
+            direction = Vector2.zero;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        pm.Move(direction);
+    }
+
+    private void TakeInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        direction = new Vector2(moveX, moveY);
+        Debug.Log("X: " + direction.x + "Y: "+direction.y);
+
+        if (direction.x != 0 || direction.y != 0)
+        {
+            AttackDir = direction;
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+            changeSprite();
+        if (Input.GetKeyDown(KeyCode.Space))
+            Attack();
+    }
+
+    public void changeSprite()
+    {
+        if (isSword)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = arco;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = sword;
+        }
+        animator.SetBool("hasSword", !animator.GetBool("hasSword"));
+    }
+
+    public void Attack()
+    {
+        if (animator.GetBool("hasSword"))
+        {
+            attackPoint.position = new Vector3(this.transform.position.x + (AttackDir.x * multiplier), this.transform.position.y + (AttackDir.y * multiplier), 0);
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<EnemyRecieveDamage>().DealDamage(damage);
+            }
+        }
+        else
+        {
+            float angulo = 0;
+            if (AttackDir.x != 0)
+                angulo = Mathf.Acos(AttackDir.x);
+            else
+                angulo = Mathf.Asin(AttackDir.y);
+            angulo = angulo * 180 / Mathf.PI;
+            GameObject arrow_thrown = Instantiate(arrow, transform.position, Quaternion.Euler(0, 0, angulo));
+            arrow_thrown.GetComponent<Rigidbody2D>().velocity = AttackDir * projectileForce;
+        }
+
+        imAttacking = true;
+        animator.SetTrigger("Attack");
+
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 
 
-    public void takeDamage()
+
+    public void takeDamage(int damage)
     {
-        currentHealth--; //Si llega a 0, muere
-        healthbar.setHealth(currentHealth);
+        currentHealth-=damage; //Si llega a 0, muere
+        canvas.GetComponent<DisplayManagement>().setHealth(currentHealth);
     }
     public void heal()
     {
         currentHealth = maxHealth;
-        healthbar.setHealth(maxHealth);
+        canvas.GetComponent<DisplayManagement>().setHealth(maxHealth);
     }
-    
+
+    public bool CheckAttack()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_Sword") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_Bow"))
+            imAttacking = false;
+        return imAttacking;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,150 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Start is called before the first frame update
     public float speed;
-    private Vector2 direction;
     private Vector2 AttackDir;
     private Animator animator;
 
-    private SpriteRenderer spriterenderer;
-    public Sprite arco;
-    public Sprite sword;
-    private bool isSword = true;
-    private bool imAttacking = false;
-
-    public float attackRange = 0.5f;
-    public LayerMask enemyLayers;
-    private Transform attackPoint;
-    public float multiplier;
-    public int damage = 20;
-
-    public GameObject arrow;
-    public float projectileForce = 2f;
     private Rigidbody2D rb;
-  
 
-
-    // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        AttackDir = Vector2.zero;
-        spriterenderer = this.GetComponent<SpriteRenderer>();
-        spriterenderer.sprite = sword;
-        GameObject newGameObject = new GameObject();
-        attackPoint = newGameObject.transform;
-
     }
 
     // Update is called once per frame
-    void Update()
+    public void Move(Vector2 Direction)
     {
-        if(!CheckAttack())
-        {
-            TakeInput();
-        }
-        else
-        {
-            direction = Vector2.zero;
-        }
-    }
-    
-    void FixedUpdate()
-    {
-        Move();
-    }
+        rb.velocity = new Vector2(Direction.x * speed, Direction.y * speed);
 
-    private void Move()
-    {
-        rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
-        if (direction.x != 0 || direction.y != 0)
-            SetAnimatorMovement(direction);
+        if (Direction.x != 0 || Direction.y != 0) // se mueve
+        {
+            SetAnimatorMovement(Direction);
+            AttackDir = Direction;
+        }
         else
             SetAnimatorMovement(AttackDir);
     }
-    private void TakeInput()
-    {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
-        direction = new Vector2(moveX,moveY);
-        
-        if (direction.x != 0 || direction.y != 0)
-        {
-            AttackDir = direction;
-            animator.SetBool("isMoving", true);
-        }
-        else
-        {
-            animator.SetBool("isMoving", false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-            changeSprite();
-        if (Input.GetKeyDown(KeyCode.Space))
-            Attack();
-    }
-    
     private void SetAnimatorMovement(Vector2 direction)
     {
 
         animator.SetFloat("xDir", direction.x);
         animator.SetFloat("yDir", direction.y);
     }
-
-
-    public void changeSprite()
-    {
-        if (isSword)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = arco;
-        }
-        else
-        {
-            this.GetComponent<SpriteRenderer>().sprite = sword;
-        }
-        animator.SetBool("hasSword", !animator.GetBool("hasSword"));
-    }
-
-    public void Attack()
-    {
-        if(animator.GetBool("hasSword"))
-        {
-            attackPoint.position= new Vector3(this.transform.position.x + (AttackDir.x * multiplier), this.transform.position.y + (AttackDir.y * multiplier),0);
-            Collider2D[] hitEnemies=Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-            foreach(Collider2D enemy in hitEnemies)
-            {
-                enemy.GetComponent<EnemyRecieveDamage>().DealDamage(damage);
-            }
-        }
-        else
-        {
-            float angulo = 0;
-            if (AttackDir.x != 0)
-                angulo = Mathf.Acos(AttackDir.x);
-            else
-                angulo = Mathf.Asin(AttackDir.y);
-            angulo = angulo * 180 / Mathf.PI;
-            GameObject arrow_thrown = Instantiate(arrow, transform.position, Quaternion.Euler(0, 0, angulo));
-            arrow_thrown.GetComponent<Rigidbody2D>().velocity = AttackDir * projectileForce;
-        }
-
-        imAttacking = true;
-        animator.SetTrigger("Attack");
-        
-    }
-    public bool CheckAttack()
-    {
-       if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_Sword")&& !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_Bow"))
-            imAttacking = false;
-        return imAttacking;
-    }
-
-    public void OnDrawGizmosSelected()
-    {
-        if (attackPoint == null)
-            return;
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-    }
-
 }
